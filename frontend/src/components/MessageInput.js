d135d00dfaab30b6ae4013692e9868bf3f834b69
willfbren <willfbren@gmail.com>
2020-06-17 16:05:43 -0500

    emoji support

--- frontend/src/components/MessageInput.js
+++ frontend/src/components/MessageInput.js
@@ -4,32 +4,30 @@ import {
     Input,
     InputGroup,
     InputRightElement,
-    IconButton,
+    InputLeftElement,
+    IconButton
 } from "@chakra-ui/core";
 import { useSelector } from "react-redux";
+import { FaRegSmile } from 'react-icons/fa'
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 
 function MessageInput() {
     const user = useSelector((state) => state.currentUser);
     const channel = useSelector((state) => state.currentChannel);
 
     const messageState = {
-        name: null,
-        avatar: null,
-        user_id: null,
-        channel_id: null,
+        name: user.name,
+        avatar: user.avatar,
+        user_id: user.id,
+        channel_id: channel.id,
         content: "",
     };
 
     const [message, setMessage] = useState(messageState);
 
     let setValue = (e) => {
-        setMessage({
-            name: user.name,
-            avatar: user.avatar,
-            user_id: user.id,
-            channel_id: channel.id,
-            content: e.target.value,
-        });
+        setMessage({...message, content: e.target.value });
     };
 
     let handleSubmit = (e) => {
@@ -50,9 +48,33 @@ function MessageInput() {
         setMessage(messageState);
     };
 
+    const [emojiPickerState, SetEmojiPicker] = useState(false);
+  
+    let emojiPicker;
+    if (emojiPickerState) {
+      emojiPicker = (
+        <Picker
+          title="Pick your emoji…"
+          emoji="point_up"
+          onSelect={emoji => pickEmoji(emoji)}
+        />
+      );
+    }
+
+    let pickEmoji = (emoji) => {
+        setMessage({...message, content: message.content + emoji.native})
+        return triggerPicker()
+    }
+  
+    function triggerPicker(event) {
+      SetEmojiPicker(!emojiPickerState);
+    }
+
     return (
         <Box pl="15px" pr="15px" pt="15px">
+            <Box pos="absolute" left="0" bottom="0">{emojiPicker}</Box>
             <InputGroup>
+            <InputLeftElement children={<Box as={FaRegSmile} color="gray.300" cursor="pointer" onClick={triggerPicker}/>} />
                 <Input
                     placeholder="Message..."
                     value={message.content}
